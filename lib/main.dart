21a2c1432669dfdb9820ae4beae7066ec4d38cdc
Luis Moromisato <moromisato.luis@gmail.com>
2021-06-21 21:08:19 -0300

    renamed some variables

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'PayFlow',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'PayFlow'),
     );
   }
 }
