7e5454802cf3cc53c4a0671881eb8e71dfd69ac7
Luis Moromisato <moromisato.luis@gmail.com>
2021-06-22 21:31:04 -0300

    changed main to load fire base

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,49 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:nlw6_payflow/models/splash/splash_page.dart';
-import 'package:nlw6_payflow/shared/themes/app_colors.dart';
+import 'package:nlw6_payflow/app_widget.dart';
 
 void main() {
-  runApp(AppWidget());
+  runApp(AppFireBase());
 }
 
-class AppWidget extends StatelessWidget {
+class AppFireBase extends StatefulWidget {
+  @override
+  _AppFireBaseState createState() => _AppFireBaseState();
+}
+
+class _AppFireBaseState extends State<AppFireBase> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'PayFlow',
-      theme: ThemeData(
-        primaryColor: AppColors.primary,
-      ),
-      home: SplashPage(),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Material(
+            child: Center(
+              child: Text(
+                "Não foi possível inicializar o Firebase",
+                textDirection: TextDirection.ltr,
+              ),
+            ),
+          );
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return AppWidget();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Material(
+          child: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      },
     );
   }
 }
