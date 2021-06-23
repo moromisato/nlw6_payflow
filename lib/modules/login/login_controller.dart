b8f88c68c9cb34704deb01725f69ee197b19c517
Luis Moromisato <moromisato.luis@gmail.com>
2021-06-22 22:28:52 -0300

    called set user function

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:nlw6_payflow/shared/auth/auth_controller.dart';
+import 'package:nlw6_payflow/shared/models/user_model.dart';
 
 class LoginController {
   final authController = AuthController();
@@ -10,7 +11,9 @@ class LoginController {
     ]);
     try {
       final response = await _googleSignIn.signIn();
-      authController.setUser(context, response);
+      final user =
+          UserModel(name: response!.displayName!, photoUrl: response.photoUrl);
+      authController.setUser(context, user);
       print(response);
     } catch (error) {
       authController.setUser(context, null);
