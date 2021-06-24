18e9fe65802139af69b7a2a2b6a6e096d8476718
Luis Moromisato <moromisato.luis@gmail.com>
2021-06-23 21:43:39 -0300

    added new barcode scanner route added settings to lock page orientation change

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:nlw6_payflow/modules/barcode_scanner/barcode_scanner_page.dart';
 
 import 'modules/home/home_page.dart';
 import 'modules/login/login_page.dart';
@@ -6,18 +8,26 @@ import 'modules/splash/splash_page.dart';
 import 'shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'PayFlow',
       theme: ThemeData(
+        primarySwatch: Colors.orange,
         primaryColor: AppColors.primary,
       ),
       initialRoute: "/splash",
       routes: {
         "/splash": (context) => SplashPage(),
         "/home": (context) => HomePage(),
-        "/login": (context) => LoginPage()
+        "/login": (context) => LoginPage(),
+        "/barcode_scanner": (context) => BarcodeScannerPage()
       },
     );
   }
