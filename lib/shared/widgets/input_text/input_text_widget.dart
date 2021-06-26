812f6ed6775a3929e09e5a3777fcb8bea3f08376
Luis Moromisato <moromisato.luis@gmail.com>
2021-06-26 17:47:55 -0300

    added animation to input text

--- lib/shared/widgets/input_text/input_text_widget.dart
+++ lib/shared/widgets/input_text/input_text_widget.dart
@@ -1,3 +1,4 @@
+import 'package:animated_card/animated_card.dart';
 import 'package:flutter/material.dart';
 import 'package:nlw6_payflow/shared/themes/app_colors.dart';
 import 'package:nlw6_payflow/shared/themes/app_text_styles.dart';
@@ -22,41 +23,48 @@ class InputTextWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 16),
-      child: Column(
-        children: [
-          TextFormField(
-            style: TextStyles.input,
-            decoration: InputDecoration(
-                contentPadding: EdgeInsets.zero,
-                labelText: label,
-                labelStyle: TextStyles.input,
-                border: InputBorder.none,
-                icon: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 18),
-                      child: Icon(
-                        icon,
-                        color: AppColors.primary,
+    return AnimatedCard(
+      direction: AnimatedCardDirection.right,
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 16),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: controller,
+              initialValue: initialValue,
+              validator: validator,
+              onChanged: onChanged,
+              style: TextStyles.input,
+              decoration: InputDecoration(
+                  contentPadding: EdgeInsets.zero,
+                  labelText: label,
+                  labelStyle: TextStyles.input,
+                  border: InputBorder.none,
+                  icon: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 18),
+                        child: Icon(
+                          icon,
+                          color: AppColors.primary,
+                        ),
                       ),
-                    ),
-                    Container(
-                      width: 1,
-                      height: 48,
-                      color: AppColors.stroke,
-                    ),
-                  ],
-                )),
-          ),
-          Divider(
-            height: 1,
-            thickness: 1,
-            color: AppColors.stroke,
-          )
-        ],
+                      Container(
+                        width: 1,
+                        height: 48,
+                        color: AppColors.stroke,
+                      ),
+                    ],
+                  )),
+            ),
+            Divider(
+              height: 1,
+              thickness: 1,
+              color: AppColors.stroke,
+            )
+          ],
+        ),
       ),
     );
   }
